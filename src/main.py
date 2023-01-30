2400ac290db9ee6e5600468bc916cdc6e7b35ea9
lebelius <alexeevroma@gmail.com>
2023-01-30 19:53:27 +0300

    feat: add launch to main and configure logger

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import logging
+from src.core.application import Application
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
+    logger.info("Starting Applicaction")
+    app = Application()
+    app.run()
