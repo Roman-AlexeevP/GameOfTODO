18dbb8b5fde7ef898b323c9270dc0549a8890f4a
lebelius <alexeevroma@gmail.com>
2023-01-30 19:30:58 +0300

    feat: write init sql script for tasks table

---
+++ src/db/sql_sqripts/init_tasks.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    hours_to_complete INTEGER NOT NULL ,
+    priority_type INTEGER NOT NULL ,
+    ended_at DATETIME,
+    is_active TINYINT NOT NULL DEFAULT 0,
+    is_complete TINYINT NOT NULL DEFAULT 0,
+    created_at DATETIME NOT NULL,
+    worked_hours INTEGER
+
+)
