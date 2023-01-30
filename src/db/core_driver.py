ac1124571b886be2d5c82e7b6cf8c498ec9ea900
lebelius <alexeevroma@gmail.com>
2023-01-30 19:30:41 +0300

    feat:  add class DbDriver for connecting to sqlite

---
+++ src/db/core_driver.py
@@ -0,0 +1,37 @@
+import sqlite3
+import logging
+
+from ..core.general import Any
+from . import consts
+
+logger = logging.getLogger(__name__)
+
+
+class DbDriver(Any):
+
+    def __init__(self, db_name: str = consts.DB_NAME):
+        self.db_name = db_name
+
+    def _create_connection(self) -> sqlite3.Connection:
+        connection = sqlite3.connect(
+            database=self.db_name,
+            detect_types=sqlite3.PARSE_DECLTYPES |
+                         sqlite3.PARSE_COLNAMES
+        )
+        return connection
+
+    def _create_cursor(self, connection: sqlite3.Connection) -> sqlite3.Cursor:
+        return connection.cursor()
+
+    def query(self, sql_query: str):
+        connection = self._create_connection()
+        cursor = self._create_cursor(connection)
+        try:
+            result = cursor.execute(sql_query)
+        except sqlite3.Error as exception:
+            logger.exception(exception)
+        else:
+            return result
+        finally:
+            connection.commit()
+            connection.close()
